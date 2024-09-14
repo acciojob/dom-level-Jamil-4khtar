34256b8cdf12483f2bffec001cb12fedc3d72abf
Jamil-4khtar <jamil4khtar@gmail.com>
2024-09-14 15:18:16 +0530

    try submit

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let li = document.getElementById("level");
+let li_parent = li.parentNode;
+// console.log("🚀 ~ li_parent:", li_parent)
+
+let c = 0;
+while (li_parent != null) {
+    li_parent = li_parent.parentNode;
+    // console.log("🚀 ~ li_parent:", li_parent)
+    // console.log("🚀 ~ c:", c)  
+    c++;
+}
+alert("level of the element is: " + c)
